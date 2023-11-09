85e7c86ea1b88c4f1ea031b63a38e5553b6acee4
Bobby Singh <b830103@gmail.com>
2023-11-09 14:39:53 +0100

    debugging / improvements
    Cant figure out why I get error
    TypeError: setSelectedMovie is not a function
    
    when clicking on simiular movies

--- src/components/similar-movies-view/similar-movies-view.js
+++ src/components/similar-movies-view/similar-movies-view.js
@@ -2,30 +2,45 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import { MovieCard } from "../movie-card/movie-card";
 
-export const SimilarMoviesView = ({ movies, selectedMovie }) => {
+export const SimilarMoviesView = ({
+  movies,
+  selectedMovie,
+  setSelectedMovie,
+}) => {
   return (
-    <Row className="justify-content-md-center">
+    <Row>
       <Col md={12}>
+        <hr />
         <h2 className="mb-3">Similar Movies</h2>
       </Col>
-      {movies
-        .filter(
-          (movie) =>
-            movie.id !== selectedMovie.id && movie.genre === selectedMovie.genre
-        )
-        .map((movie) => (
-          <Col
-            key={movie.id}
-            xs={12}
-            s={10}
-            md={6}
-            lg={4}
-            xl={3}
-            className="mb-5"
-          >
-            <MovieCard movie={movie} />
-          </Col>
-        ))}
+
+      <Row className="justify-content-md-center">
+        {movies
+          .filter(
+            (movie) =>
+              movie.id !== selectedMovie.id &&
+              movie.genre === selectedMovie.genre
+          )
+          .map((movie) => (
+            <Col
+              key={movie.id}
+              xs={12}
+              sm={10}
+              md={6}
+              lg={4}
+              xl={3}
+              className="mb-5"
+            >
+              <MovieCard
+                key={movie.id}
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => {
+                  setSelectedMovie(newSelectedMovie);
+                }}
+              />
+            </Col>
+          ))}
+      </Row>
     </Row>
   );
 };
