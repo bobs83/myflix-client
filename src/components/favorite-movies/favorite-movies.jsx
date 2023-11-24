754e21823eb7d99fcc8678111b7551c93f1ba789
Bobby Singh <b830103@gmail.com>
2023-11-24 15:53:49 +0100

    Upgrades to favorite movies UI

--- src/components/favorite-movies/favorite-movies.jsx
+++ src/components/favorite-movies/favorite-movies.jsx
@@ -1,23 +1,42 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Alert } from "react-bootstrap";
 import { MovieCard } from "../movie-card/movie-card";
 import "../movie-card/movie-card.scss";
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
 
 //creates an array with all the movies
 
-export const FavoriteMovies = ({ user, token, setUser, movies }) => {
-  console.log(movies);
-  // seems to work fine
-  console.log(user);
-  // seems to work fine
-  console.log(user.FavoriteMovies);
-  // undefined sometimes, but sometimes it works fine, (when i am not connected with server?)
+export const FavoriteMovies = ({ token, movies }) => {
+  const [user, setUser] = useState(null);
 
-  //user.FavoriteMovie = user.FavoriteMovies;
-  let result = movies.filter((movie) => user.FavoriteMovies.includes(movie.id));
+  useEffect(() => {
+    // Load user data from local storage
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, [user]);
 
-  if (result.length === 0) return <p>You have no favorite movies yet!</p>;
+  let result = movies.filter(
+    (movie) => user && user.FavoriteMovies.includes(movie.id)
+  );
 
+  if (result.length === 0) {
+    return (
+      <Container>
+        <Row className="justify-content-center mt-5">
+          <Col xs={12} sm={10} md={8} lg={6}>
+            <Alert variant="info" className="text-center py-4">
+              <FontAwesomeIcon icon={faFilm} size="3x" className="mb-3" />
+              <h4>You have no favorite movies yet!</h4>
+            </Alert>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
   return (
     <>
       <Container>
