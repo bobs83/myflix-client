365166a3d8a991116aefaa7389e3eb2195e7473d
Bobby Singh <b830103@gmail.com>
2023-11-26 21:16:27 +0100

    Debugging

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {SearchView } from "../search-view/search-view
+import {SearchView } from "../search-view/search-view";
 import {
   Navbar,
   Nav,
